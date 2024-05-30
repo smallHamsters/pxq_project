bafddea34cfd64dff8816c69a58ab4c9ba818cab
dino-pc <muhyangestate@naver.com>
2024-05-30 23:29:45 +0900

    1. 공정위문구 단어검색 완성  => 기능개발, 모듈화 완성  => dataset 내용 수정 필요

---
+++ public/js/st-keyword.js
@@ -0,0 +1,10 @@
+const result_obj_length = 53;
+
+for (let i = 0; i < result_obj_length; i++) {
+  document
+    .querySelectorAll(".st-keyword-detail-toggle")
+    [i].addEventListener("click", function () {
+      document.querySelectorAll(".list-detail")[i].classList.toggle("show");
+      document.querySelectorAll(".list-d")[i].classList.toggle("bg-green");
+    });
+}
--- routes/sponsoredtag.js
+++ routes/sponsoredtag.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const path = require("path");
+
+const FILE_NAME = "st-keywordDATA.csv";
+const csvPath = path.join(__dirname, "..", "public", "data", FILE_NAME);
+const CSVfile = fs.readFileSync(csvPath, "utf-8"); //CSVfile 내부 데이터 전부 읽기
+// console.log(CSVfile);
+
+const rows = CSVfile.split("\r\n"); //CSVfie 내부 데이터 전부 행으로 자름
+// console.log(rows);
+
+const row2 = rows[1].split(","); // 내부 데이터를 ,별로 잘라 list에 담은 행 1개
+// console.log(row2);
+
+let result = [];
+for (let i = 1; i < rows.length; i++) {
+  result.push(rows[i].split(","));
+}
+
+// console.log(result);
+
+let result_obj = result.map((subArray) => ({
+  숫자: subArray[0],
+  단어: subArray[1],
+  사용가부: subArray[2],
+  사용제한: subArray[3],
+  불가사유: subArray[4],
+  사용예시: subArray[5],
+  해결방법: subArray[6],
+  주의사항: subArray[7],
+}));
+
+// console.log(result_obj.length);
+
 router.get("", (req, res) => {
   res.render("./sponsoredtags/st-main.ejs");
 });
@@ -10,7 +44,9 @@ router.get("/checker", (req, res) => {
 });
 
 router.get("/keyword", (req, res) => {
-  res.render("./sponsoredtags/st-keyword.ejs");
+  res.render("./sponsoredtags/st-keyword.ejs", {
+    result_obj: result_obj,
+  });
 });
 
 router.get("/sentence", (req, res) => {
