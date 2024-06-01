04502eb14735e7d5152c7070d67ca0cf4e161b1c
dino-pc <muhyangestate@naver.com>
2024-06-01 19:18:42 +0900

    1. 에러 핸들링 완료 (1) errorhandler 모듈화 (2) httpStatus 401, 402, 403, 404, 500 대응

---
+++ middlewares/errorhandler.js
@@ -0,0 +1,34 @@
+function errorhandler(err, req, res, next) {
+  let statusCode = err.status || 500;
+  let message;
+
+  switch (statusCode) {
+    case 401:
+      message = "Unauthorized: Access is denied due to invalid credentials.";
+      break;
+    case 402:
+      message =
+        "Payment Required: Payment is required to access this resource.";
+      break;
+    case 403:
+      message =
+        "Forbidden: You do not have permission to access this resource.";
+      break;
+    case 404:
+      message = "Not Found: The requested resource could not be found.";
+      break;
+    case 500:
+    default:
+      message =
+        "Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request.";
+      break;
+  }
+
+  res.status(statusCode);
+  res.render("errors/error.ejs", {
+    status: statusCode,
+    message: message,
+  });
+}
+
+module.exports = errorhandler;
