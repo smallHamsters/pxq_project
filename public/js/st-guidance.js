542acc3f80022f38b0fd0dc7fb6bcfd20fd495ae
dino-pc <muhyangestate@naver.com>
2024-06-07 19:54:51 +0900

    1. 공정위문구 상황별 추천문구 구현 완성 : 이제 디자인만 다듬으면 됨!!

--- public/js/st-guidance.js
+++ public/js/st-guidance.js
@@ -1,14 +1,36 @@
-const result_obj_guidance_length = 13;
 const relationInput = document.querySelectorAll(".relation input");
 const rExample = document.querySelectorAll(".r-example");
 let currentShownIndex = null;
+
 for (let i = 0; i < relationInput.length; i++) {
   relationInput[i].addEventListener("click", function () {
     if (currentShownIndex !== null) {
       rExample[currentShownIndex].classList.remove("show");
     }
 
-    rExample[i].classList.toggle("show");
-    currentShownIndex = i;
+    if (currentShownIndex !== i) {
+      rExample[i].classList.add("show");
+      currentShownIndex = i;
+    } else {
+      currentShownIndex = null;
+    }
+  });
+}
+
+const platformInput = document.querySelectorAll(".platform input");
+const pExample = document.querySelectorAll(".p-example");
+let currentPhotoIndex = null;
+
+for (let i = 0; i < platformInput.length; i++) {
+  platformInput[i].addEventListener("click", function () {
+    if (currentPhotoIndex !== null) {
+      pExample[currentPhotoIndex].classList.remove("show");
+    }
+    if (currentPhotoIndex !== i) {
+      pExample[i].classList.add("show");
+      currentPhotoIndex = i;
+    } else {
+      currentPhotoIndex = null;
+    }
   });
 }
