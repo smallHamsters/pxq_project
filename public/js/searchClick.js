9a5df071eb50e6649df22f1dbf9989eee9f1505a
dino-pc <muhyangestate@naver.com>
2024-06-09 18:50:52 +0900

    1. navbar에 logo (favicon.png) 장착
    2. main page 컴포넌트 일부 변경 완료

---
+++ public/js/searchClick.js
@@ -0,0 +1,9 @@
+// window.onload = function () {
+//   document.querySelector(".search-send").addEventListener("click", function () {
+//     let searchTerm = document.querySelector(".st-search").value;
+//     if (searchTerm !== "") {
+//       location.href = "/check?val=" + searchTerm;
+//     }
+//     console.log(searchTerm);
+//   });
+// };
