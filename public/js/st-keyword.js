f410241efae7dab4e65fab19705ff47e497c36fe
dino-pc <muhyangestate@naver.com>
2024-06-13 18:02:22 +0900

    dictionary page에 animation 효과 추가

--- public/js/st-keyword.js
+++ public/js/st-keyword.js
@@ -4,7 +4,9 @@ for (let i = 0; i < result_obj_length; i++) {
   document
     .querySelectorAll(".st-keyword-detail-toggle")
     [i].addEventListener("click", function () {
-      document.querySelectorAll(".list-detail")[i].classList.toggle("show");
+      document
+        .querySelectorAll(".list-detail")
+        [i].classList.toggle("slide-down-show");
       document.querySelectorAll(".list-d")[i].classList.toggle("bg-blue100");
     });
 }
